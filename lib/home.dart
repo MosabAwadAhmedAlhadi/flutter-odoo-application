4bf53f4ce5d649f581d21de54ceb0b1cd8459cb8
lenovo <mosab.awad.ahmed.alhadi@gmail.com>
2021-05-23 02:15:21 -0700

    adding two taps and adding buttons to them with fake actions as a first step

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-
+import 'package:mass_project/main.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       home: DefaultTabController(
         length: 2,
         child: Scaffold(
             appBar: AppBar(
               bottom: TabBar(
                 tabs: [
-                  Tab(icon: Icon(Icons.directions_car)),
-                  Tab(icon: Icon(Icons.directions_transit)),
+                  Tab(text:"Properity Management"),
+                  Tab(text:"Contracts and Payments"),
                 ],
               ),
               title: Text("Dashboard"),
@@ -19,7 +20,8 @@ class HomePage extends StatelessWidget {
             body: TabBarView(
               children: [
                 HomePageContent(),
-                Icon(Icons.directions_transit),
+                ContractsManagement(),
+
               ],
             ),
           ),
@@ -37,13 +39,80 @@ class _HomePageContentState extends State<HomePageContent> {
   @override
   Widget build(BuildContext context) {
     // here we will return our home page content later
+
     return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Text('Go back!'),
-      ),
+      child:GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: [
+              Container(
+                child: ElevatedButton(
+                  child: Text("Properity"),
+                  onPressed:(){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
+                    },
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  child: Text("Units"),
+                  onPressed:(){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
+                  }
+                  ,
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  child: Text("Customers"),
+                  onPressed:(){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
+                  }
+                  ,
+                ),
+              ),
+            ],
+          )
+    );
+  }
+}
+
+class ContractsManagement extends StatefulWidget {
+  @override
+  _ContractsManagementState createState() => _ContractsManagementState();
+}
+
+class _ContractsManagementState extends State<ContractsManagement> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child:GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          children: [
+            Container(
+              child: ElevatedButton(
+                child: Text("Contracts"),
+                onPressed:(){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
+                },
+              ),
+            ),
+            Container(
+              child: ElevatedButton(
+                child: Text("Payments"),
+                onPressed:(){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
+                }
+                ,
+              ),
+            ),
+          ],
+        )
     );
   }
 }
